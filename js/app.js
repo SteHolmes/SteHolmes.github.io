e22a7eb184ecd1329f4eb3c9d77988ae30131940
Steve Holmes <steveholmesart@outlook.com>
2019-05-14 19:36:40 +0000

    Refactor navbar scroll feature

--- js/app.js
+++ js/app.js
@@ -3,27 +3,14 @@ $(document).ready(function(){
 
 // TOP NAV MENU SCROLLING FUNCTION //
 
-
-  $(".about").click(function() {
-      $('html, body').animate({
-        scrollTop: $("#about").offset().top
+  $("nav a").click(scroll);
+  
+  function scroll() {
+    var id = $(this).text().toLowerCase();
+    $('html, body').animate({
+        scrollTop: $("#" + id).offset().top -20
       }, 1000);
-  });  
-  $(".projects").click(function() {
-      $('html, body').animate({
-        scrollTop: $("#projects").offset().top
-      }, 1000);
-  });
-  $(".skills").click(function() {
-      $('html, body').animate({
-        scrollTop: $("#skills").offset().top
-      }, 1000);
-  });
-  $(".contact").click(function() {
-      $('html, body').animate({
-        scrollTop: $("#contact").offset().top
-      }, 1000);
-  });
+  }
   
 // TOP NAV BACKGROUND COLOUR FUNCTION // 
 
