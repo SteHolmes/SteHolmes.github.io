595863d1fecdfbe0dfb608bcced92d883e33bfe6
Steve Holmes <steveholmesart@outlook.com>
2019-05-23 17:46:14 +0000

    Add scroll reveal feature

--- js/app.js
+++ js/app.js
@@ -28,4 +28,19 @@ $(document).ready(function(){
       }
   });
 
+// REVEAL ELEMENTS ON SCROLL //
+
+  $(window).scroll(function () {
+        var revealEl = $(".scroll-reveal");
+        var currScrollPos = $(window).scrollTop() + $(window).height();
+      
+      revealEl.each(function() {
+          var $this = $(this),
+              elemOffsetTop = $this.offset().top + 20;
+          
+          if (currScrollPos > elemOffsetTop) 
+              $this.css("opacity", 0 + (currScrollPos - elemOffsetTop)/400);
+      })
+    })
+
 });
